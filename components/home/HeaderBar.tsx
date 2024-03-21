e9b0634073bf49811f894464dc15efc922d2df90
banahaker <pn0818x@outlook.com>
2024-03-21 19:26:42 +0800

    style: Update HeaderBar width full

--- components/home/HeaderBar.tsx
+++ components/home/HeaderBar.tsx
@@ -2,7 +2,7 @@ import { HeaderMenuLinks } from "@/data/HeaderMenuLinks";
 
 export default function HeaderBar() {
   return (
-    <div className="header fixed flex flex-row justify-between z-[9999] bg-slate-50 opacity-80 px-6 py-3">
+    <div className="header fixed w-full flex flex-row justify-between z-[9999] bg-slate-50 opacity-80 px-6 py-3">
       <div className="logo">MDARTEXH</div>
       <div className="menu flex flex-row gap-2">
         {HeaderMenuLinks.map((link, index) => {
