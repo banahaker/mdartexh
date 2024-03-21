6a4b5f1347dd8b9f2698cfffe9fc02e0a01decfb
banahaker <pn0818x@outlook.com>
2024-03-21 19:36:32 +0800

    feat: home page design content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className=" h-[100dvh] bg-[url('/images/sample_picture.jpg')] bg-center bg-cover">
-      <h1 className="text-3xl font-bold">你好，世界</h1>
+    <div className="h-[100dvh] bg-[url('/images/sample_picture.jpg')] bg-center bg-cover flex flex-col items-center justify-center gap-2">
+      <h1 className="text-6xl font-bold text-white">你好，世界</h1>
+      <h2 className="text-2xl text-gray-300 font-medium">
+        明道中學高中美展 7th
+      </h2>
     </div>
   );
 }
