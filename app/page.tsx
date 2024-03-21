e6dbb53b527d686a98b6281d45d2742d53618132
banahaker <pn0818x@outlook.com>
2024-03-21 19:26:47 +0800

    style: Update page background image and text size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
+    <div className=" h-[100dvh] bg-[url('/images/sample_picture.jpg')] bg-center bg-cover">
       <h1 className="text-3xl font-bold">你好，世界</h1>
-    </>
+    </div>
   );
 }
