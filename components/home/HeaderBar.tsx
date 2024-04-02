31912ad2076c5e51f60e68d424cc0f16ef12a7ba
banahaker <pn0818x@outlook.com>
2024-04-02 23:02:57 +0800

    style: Update header bar styling

--- components/home/HeaderBar.tsx
+++ components/home/HeaderBar.tsx
@@ -2,14 +2,14 @@ import { HeaderMenuLinks } from "@/data/HeaderMenuLinks";
 
 export default function HeaderBar() {
   return (
-    <div className="header fixed w-full flex flex-row justify-between z-[9999] bg-black opacity-60 px-6 py-3 text-white">
+    <div className="header fixed w-[80dvw] top-[1rem] left-[50%] translate-x-[-50%] flex flex-row justify-between z-[9999] backdrop-blur-md bg-white/30 px-8 py-4 text-white rounded-full border border-white">
       <div className="logo">MDARTEXH</div>
       <div className="menu flex flex-row gap-3">
         {HeaderMenuLinks.map((link, index) => {
           return (
             <div className="menu-item" key={index}>
               <a
-                className="hover:text-cyan-500 transition uppercase"
+                className="hover:text-cyan-900 transition uppercase"
                 href={link.href}
               >
                 {link.name}
