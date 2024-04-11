c8b0a45e92710f67fd8a3a7fe1f9d0504ffe0882
banahaker <pn0818x@outlook.com>
2024-04-11 17:35:09 +0800

    feat: Update Work component with new picture and information

--- components/gallery/Work.tsx
+++ components/gallery/Work.tsx
@@ -4,12 +4,14 @@ export default function Work() {
   return (
     <>
       <div className="work-outer bg-slate-50 w-[100dvw] h-[100dvh] relative overflow-hidden">
-        <div className="work-picture bg-slate-50 bg-[url('/images/sample_picture_2.jpg')] w-full h-full"></div>
-        <div className="work-info backdrop-blur-sm bg-white/10 px-8 py-4 text-white rounded-3xl absolute bottom-6 left-1/2 -translate-x-1/2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos libero,
-          autem nesciunt tempora nostrum enim repellat, dolore porro
-          necessitatibus culpa earum architecto amet, dolorum a voluptate
-          exercitationem numquam et illo!
+        <div className="work-picture bg-slate-50 bg-[url('/images/sample_work.jpeg')] w-full h-full"></div>
+        <div className="work-info backdrop-blur-sm bg-white/20 px-8 py-4 text-white rounded-3xl absolute bottom-6 left-1/2 -translate-x-1/2">
+          <div className="author-name">陳凱恩</div>
+          <div className="work-name">The Deathly Hallows </div>
+          <div className="category">紙雕燈</div>
+          <div className="description">
+            此件作品以電影Harry Potter中所提及的「死神的聖物」作為發想。
+          </div>
         </div>
       </div>
     </>
