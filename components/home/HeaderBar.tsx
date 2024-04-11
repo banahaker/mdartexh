80bd59213fb676d9c69c532b145249e8920189ee
banahaker <pn0818x@outlook.com>
2024-04-11 23:05:39 +0800

    style: Update HeaderBar and Home.scss styles
    - Updated the HeaderBar component style to include a max-width of 1024px.
    - Added a filter of brightness(0.5) to the .work-picture class in Home.scss.

--- components/home/HeaderBar.tsx
+++ components/home/HeaderBar.tsx
@@ -2,7 +2,7 @@ import { HeaderMenuLinks } from "@/data/HeaderMenuLinks";
 
 export default function HeaderBar() {
   return (
-    <div className="header fixed w-[80dvw] top-[1rem] left-[50%] translate-x-[-50%] flex flex-row justify-between z-[9999] backdrop-blur-md bg-white/30 px-8 py-4 text-white rounded-full border border-white">
+    <div className="header fixed w-[80dvw] max-w-[1024px] top-[1rem] left-1/2 -translate-x-1/2 flex flex-row justify-between z-[9999] backdrop-blur-md bg-white/30 px-8 py-4 text-white rounded-full border border-white">
       <div className="logo">MDARTEXH</div>
       <div className="menu flex flex-row gap-3">
         {HeaderMenuLinks.map((link, index) => {
