339949f654f02af37f2c6a89a5e99fea568507f5
banahaker <pn0818x@outlook.com>
2024-04-18 11:07:07 +0800

    fix: Fix image link in workDatas
    The image link for one of the works was misspelled. This commit fixes the typo in the image link.
    
    Changes:
    - Corrected the image link for a work in the workDatas file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Work from "@/components/gallery/Work";
 import { works } from "@/data/workDatas";
 
 import "@/styles/home/Home.scss";
 
 export default function Home() {
+  const [currentScroll, setCurrentScroll] = useState(0);
+
+  useEffect(() => {
+    const handleWheelScroll = (event: WheelEvent) => {
+      const scrollDirection = event.deltaY;
+
+      if (scrollDirection > 0) {
+        const targetPosition = currentScroll + window.innerHeight;
+        setCurrentScroll(targetPosition);
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+        event.preventDefault();
+      } else {
+        const targetPosition = currentScroll - window.innerHeight;
+        setCurrentScroll(targetPosition);
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+        event.preventDefault();
+      }
+    };
+
+    const handleTouchScroll = (event: TouchEvent) => {
+      const touch = event.touches[0];
+      const scrollDirection = touch.pageY - currentScroll;
+
+      if (scrollDirection > 0) {
+        const targetPosition = currentScroll + window.innerHeight;
+        setCurrentScroll(targetPosition);
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+        event.preventDefault();
+      } else {
+        const targetPosition = currentScroll - window.innerHeight;
+        setCurrentScroll(targetPosition);
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+        event.preventDefault();
+      }
+    };
+
+    window.addEventListener("wheel", handleWheelScroll, { passive: false });
+    // phone scroll
+    window.addEventListener("touchmove", handleTouchScroll, { passive: false });
+  }, [currentScroll]);
+
   return (
     <>
       <div className="backgroundFilter h-[100dvh] bg-[url('/images/sample_picture.jpg')] bg-center bg-cover flex flex-col items-center justify-center gap-2">
@@ -13,7 +70,7 @@ export default function Home() {
         </h2>
       </div>
       {works.map((work) => {
-        return <Work key={work.name} {...work}></Work>;
+        return <Work key={work.link} {...work}></Work>;
       })}
     </>
   );
